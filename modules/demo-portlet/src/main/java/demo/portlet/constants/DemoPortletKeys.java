644664a0e2d80f4b3d52ef2f2aba97cb8cb4dcf3
Matt Wolinski <mwolinski@xtivia.com>
2024-02-28 20:40:31 -0700

    demo widgets

---
+++ modules/demo-portlet/src/main/java/demo/portlet/constants/DemoPortletKeys.java
@@ -0,0 +1,11 @@
+package demo.portlet.constants;
+
+/**
+ * @author mattwolinski
+ */
+public class DemoPortletKeys {
+
+	public static final String DEMO =
+		"demo_portlet_DemoPortlet";
+
+}
---
+++ modules/demo-widget/src/main/java/demo/widget/constants/DemoWidgetPortletKeys.java
@@ -0,0 +1,10 @@
+package demo.widget.constants;
+
+/**
+ * @author mattwolinski
+ */
+public class DemoWidgetPortletKeys {
+
+	public static final String DemoWidget = "demowidget";
+
+}
